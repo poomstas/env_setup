6c276e06e7859f2d68f9fe16d1ba1ed0b8882fe4
brian <brian@brian-ubuntu-22.04>
2025-04-06 14:57:52 +0900

    added neovim setup script

--- neovim_setup.sh
+++ neovim_setup.sh
@@ -1,9 +1,10 @@
-sudo apt update; sudo apt install git
+sudo apt update
+sudo apt install git
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.bashrc
 
 git clone https://github.com/LazyVim/starter $HOME/.config/nvim
 rm -rf $HOME/.config/nvim/.git
@@ -12,10 +13,28 @@ sudo apt install fd-find
 sudo apt-get install ripgrep
 sudo apt install gcc
 
-# Download and install nerd font 
+# Download and install nerd font
 sudo apt-get install unzip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip -d $HOME/Downloads/
 sudo unzip $HOME/Downloads/JetBrainsMono.zip -d $HOME/Downloads/JetBrainsMono/
+# TODO Continue writing this portion
 
+# Install node.js required for neovim Copilot
+curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt install nodejs
+
+# Add 80-char line in neovim
+echo 'vim.opt.colorcolumn = "80"' >>~/.config/nvim/init.lua # In vim, do: set colorcolumn=80
+
+# Install neovim Copilot
+
+# https://www.youtube.com/watch?v=c9y7bkk-r7u&t=92s&ab_channel=typecraft
+git clone https://github.com/github/copilot.vim \
+  ~/.config/nvim/pack/github/start/copilot.vim
+# Run below in neovim:
+# :Copilot setup
+# :Copilot enable
+
+#
 nvim
-
